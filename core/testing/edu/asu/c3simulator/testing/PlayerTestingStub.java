a86e3a4daec719a8649e0802a811def7e68e3c7d
Zach Moore <zcmoore@asu.edu>
2015-02-10 19:52:04 -0700

    Testing stubs

---
+++ core/testing/edu/asu/c3simulator/testing/PlayerTestingStub.java
@@ -0,0 +1,75 @@
+package edu.asu.c3simulator.testing;
+
+import java.text.DateFormat;
+
+import edu.asu.c3simulator.simulation.C3Simulation;
+import edu.asu.c3simulator.simulation.Player;
+import edu.asu.c3simulator.simulation.SocioeconomicStatus;
+import edu.asu.c3simulator.util.Observer;
+
+/**
+ * Stub to return default "dummy" values for testing purposes.
+ * 
+ * @author Moore, Zachary
+ *
+ */
+public class PlayerTestingStub implements Player
+{
+	@Override
+	public void registerObserver(Observer<Player> observer)
+	{
+		
+	}
+	
+	@Override
+	public SocioeconomicStatus getStatus()
+	{
+		return SocioeconomicStatus.TEST_STATUS;
+	}
+	
+	@Override
+	public C3Simulation getSimulation()
+	{
+		return new C3Simulation() {
+			@Override
+			public String getSimulationDateString()
+			{
+				return "5 Jan, Year TEST";
+			}
+			
+			@Override
+			public String getSimulationDateString(String format)
+			{
+				// TODO Auto-generated method stub return null;
+				throw new UnsupportedOperationException(
+						"The method is not implemented yet.");
+			}
+			
+			@Override
+			public String getSimulationDateString(DateFormat format)
+			{
+				// TODO Auto-generated method stub return null;
+				throw new UnsupportedOperationException(
+						"The method is not implemented yet.");
+			}
+		};
+	}
+	
+	@Override
+	public int getNetWorth()
+	{
+		return 1000000;
+	}
+	
+	@Override
+	public int getCapital()
+	{
+		return 100000;
+	}
+	
+	@Override
+	public String getID()
+	{
+		return "test";
+	}
+}
