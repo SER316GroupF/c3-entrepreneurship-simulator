c52dd411d20ae4d3f526898d7a630d6872503ff4
Zach Moore <zcmoore@asu.edu>
2015-02-10 19:50:49 -0700

    Conforming to new quality policy

--- core/src/edu/asu/c3simulator/widgets/Tables.java
+++ core/src/edu/asu/c3simulator/widgets/Tables.java
@@ -3,8 +3,24 @@ package edu.asu.c3simulator.widgets;
 import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
+/**
+ * Support class for formatting and maintaining {@link Table} objects
+ * 
+ * @author Moore, Zachary
+ * 
+ */
 public class Tables
 {
+	/**
+	 * Set the alignment of all cells in a particular column of a table
+	 * 
+	 * @param table
+	 *            Target
+	 * @param columnIndex
+	 *            Column to set the width of
+	 * @param alignment
+	 *            New alignment for all items in the specified column
+	 */
 	public static void allignColumns(Table table, int columnIndex, int alignment)
 	{
 		for (Cell<?> cell : table.getCells())
@@ -16,6 +32,16 @@ public class Tables
 		}
 	}
 	
+	/**
+	 * Set the padding of all cells in the specified column of a table
+	 * 
+	 * @param table
+	 *            Target table
+	 * @param columnIndex
+	 *            Target column
+	 * @param padding
+	 *            Padding specification
+	 */
 	public static void padColumn(Table table, int columnIndex, Padding padding)
 	{
 		for (Cell<?> cell : table.getCells())
@@ -27,6 +53,14 @@ public class Tables
 		}
 	}
 	
+	/**
+	 * Set the padding of all cells in a table
+	 * 
+	 * @param cell
+	 *            Target
+	 * @param padding
+	 *            Padding specifications
+	 */
 	public static void padAllCells(Table table, Padding padding)
 	{
 		for (Cell<?> cell : table.getCells())
@@ -35,6 +69,14 @@ public class Tables
 		}
 	}
 	
+	/**
+	 * Set the padding of a single table cell
+	 * 
+	 * @param cell
+	 *            Target
+	 * @param padding
+	 *            Padding specifications
+	 */
 	public static void padCell(Cell<?> cell, Padding padding)
 	{
 		float top = padding.get(Padding.TOP);
