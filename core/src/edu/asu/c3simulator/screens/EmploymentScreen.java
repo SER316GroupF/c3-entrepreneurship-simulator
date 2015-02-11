42102260951376a04bfbe846d022a839d307ab3b
Justin Reigel <justinreigel89@gmail.com>
2015-02-11 09:14:13 -0700

    Test Screen and Employment Screen
    used clean.xml

--- core/src/edu/asu/c3simulator/screens/EmploymentScreen.java
+++ core/src/edu/asu/c3simulator/screens/EmploymentScreen.java
@@ -20,187 +20,187 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import edu.asu.c3simulator.widgets.CornerAdvisor;
 import edu.asu.c3simulator.widgets.HomeButton;
 
-//import edu.asu.c3simulator.widgets.HomeButton;
-
-/*
- * @author Krogstad, Nick
- * Framework: Moore, Zachary 
+/**
+ * @author Krogstad, Nick Framework: Moore, Zachary
  */
-//import edu.asu.c3simulator.screens.DifficultySelectionScreen.Button2;
-//import edu.asu.c3simulator.widgets.CornerAdvisor;
 
 public class EmploymentScreen implements Screen
 {
- private static class Button2 extends TextButton
- {
-  Layout parent;
-  
-  public Button2(String text, Skin skin, Layout parent)
-  {
-   super(text, skin);
-   this.parent = parent;
-  }
-  
-  public float getPrefWidth()
-  {
-   if (parent == null)
-    return super.getPrefWidth();
-   else
-    return parent.getPrefWidth();
-  }
-  
- }
- 
- private static final int DESIGN_WIDTH = 1280;
- private static final int DESIGN_HEIGHT = 720;
- private static final int DESIGN_SCREEN_CENTER_X = DESIGN_WIDTH / 2;
- private static final int DESIGN_SCREEN_CENTER_Y = DESIGN_HEIGHT / 2;
- private static final int ROSTER_OFFSET = 400;
- private static final int PAY = 10000;
- 
- private Game game;
- private Skin skin;
- private Stage stage;
- 
- public EmploymentScreen(Game game)
- {
-  this.game = game;
-  
-  Viewport stageViewport = new StretchViewport(DESIGN_WIDTH, DESIGN_HEIGHT);
-  this.stage = new Stage(stageViewport);
-  this.skin = new Skin(Gdx.files.internal("skins/default/uiskin.json"));
-  
-  Table roster = new Table();
-  
-  Actor employeePane = createEmployeePane();
-  Actor employeeModel = createEmployeeModel();
-  
-  roster.add(employeePane).top();
-  roster.add(employeePane).top().spaceLeft(75);
-  
-  roster.add(employeeModel).top();
-  roster.add(employeeModel).top().spaceRight(75);
-  
-  roster.setTransform(true);
-  roster.setPosition(DESIGN_SCREEN_CENTER_X, DESIGN_SCREEN_CENTER_Y);
-  
-  /*
-   * CornerAdvisor advisor = new CornerAdvisor(ADVISOR_TEXT); float padding = 0.01f
-   * DESIGN_HEIGHT; float advisorLeft = DESIGN_WIDTH - advisor.getPrefWidth() -
-   * padding; float advisorBottom = DESIGN_HEIGHT - advisor.getPrefHeight() -
-   * padding; advisor.setPosition(advisorLeft, advisorBottom);
-   */
-  CornerAdvisor advisor = new CornerAdvisor("test scren");
-  float padding = 0.01f * DESIGN_HEIGHT;
-  float advisorLeft = DESIGN_WIDTH - advisor.getPrefWidth() - padding;
-  float advisorBottom = DESIGN_HEIGHT - advisor.getPrefHeight() - padding;
-  employeePane.setPosition(DESIGN_WIDTH / 4 + ROSTER_OFFSET, DESIGN_HEIGHT / 2);
-  roster.setPosition( ROSTER_OFFSET, DESIGN_HEIGHT / 2);
-  advisor.setPosition(advisorLeft, advisorBottom);
-  HomeButton home = new HomeButton();
-  
-  stage.addActor(home);
-  stage.addActor(advisor);
-  stage.addActor(roster);
-  stage.addActor(employeePane);
-  
- }
- 
- private Actor createEmployeePane()
- {
-  
-  Table employeePane = new Table();
-  
-  List<Label> employees = new LinkedList<>();
-  
-  Label employee1 = new Label("Employee 1", skin);
-  Label employee2 = new Label("Employee 2", skin);
-  Label employee3 = new Label("Employee 3", skin);
-  Label employee4 = new Label("Employee 4", skin);
-  employees.add(employee1);
-  employees.add(employee2);
-  employees.add(employee3);
-  employees.add(employee4);
-  
-  for (Label employee : employees)
-  {
-   employee.setAlignment(Align.center);
-   employeePane.add(employee);
-   employeePane.row();
-  }
-  
-  return employeePane;
- }
- 
- private Actor createEmployeeModel()
- {
-  
-  Table employeeModel = new Table();
-  
-  Label employeeName = new Label("Frank Tucker", skin);
-  Label employeePay = new Label("Pay: " + PAY, skin);
-  employeeName.setAlignment(Align.left);
-  employeePay.setAlignment(Align.left);
-  employeeModel.add(employeeName);
-  employeeModel.row();
-  employeeModel.add(employeePay);
-  
-  return employeeModel;
- }
- 
- private Actor createEmploymentTitle(Layout parent, String titleText)
- {
-  TextButton title = new Button2(titleText, skin, parent);
-  title.align(Align.center);
-  title.setDisabled(true);
-  
-  return title;
- }
- 
- @Override
- public void dispose()
- {
-  stage.dispose();
-  this.game = null;
- }
- 
- @Override
- public void hide()
- {
-  Gdx.input.setInputProcessor(null);
- }
- 
- @Override
- public void pause()
- {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("The method is not implemented yet.");
- }
- 
- @Override
- public void render(float delta)
- {
-  stage.act(delta);
-  stage.draw();
- }
- 
- @Override
- public void resize(int width, int height)
- {
-  stage.getViewport().update(width, height);
- }
- 
- @Override
- public void resume()
- {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("The method is not implemented yet.");
- }
- 
- @Override
- public void show()
- {
-  Gdx.input.setInputProcessor(stage);
- }
- 
+	private static class Button2 extends TextButton
+	{
+		Layout parent;
+
+		public Button2(String text, Skin skin, Layout parent)
+		{
+			super(text, skin);
+			this.parent = parent;
+		}
+
+		public float getPrefWidth()
+		{
+			if (parent == null)
+				return super.getPrefWidth();
+			else
+				return parent.getPrefWidth();
+		}
+
+	}
+
+	private static final int DESIGN_WIDTH = 1280;
+	private static final int DESIGN_HEIGHT = 720;
+	private static final int DESIGN_SCREEN_CENTER_X = DESIGN_WIDTH / 2;
+	private static final int DESIGN_SCREEN_CENTER_Y = DESIGN_HEIGHT / 2;
+	private static final int ROSTER_OFFSET = 400;
+	private static final int PAY = 10000;
+
+	private Game game;
+	private Skin skin;
+	private Stage stage;
+
+	public EmploymentScreen(Game game)
+	{
+		this.game = game;
+
+		Viewport stageViewport = new StretchViewport(DESIGN_WIDTH,
+				DESIGN_HEIGHT);
+		this.stage = new Stage(stageViewport);
+		this.skin = new Skin(Gdx.files.internal("skins/default/uiskin.json"));
+
+		Table roster = new Table();
+
+		Actor employeePane = createEmployeePane();
+		Actor employeeModel = createEmployeeModel();
+
+		roster.add(employeePane).top();
+		roster.add(employeePane).top().spaceLeft(75);
+
+		roster.add(employeeModel).top();
+		roster.add(employeeModel).top().spaceRight(75);
+
+		roster.setTransform(true);
+		roster.setPosition(DESIGN_SCREEN_CENTER_X, DESIGN_SCREEN_CENTER_Y);
+
+		/*
+		 * CornerAdvisor advisor = new CornerAdvisor(ADVISOR_TEXT); float
+		 * padding = 0.01f DESIGN_HEIGHT; float advisorLeft = DESIGN_WIDTH -
+		 * advisor.getPrefWidth() - padding; float advisorBottom = DESIGN_HEIGHT
+		 * - advisor.getPrefHeight() - padding; advisor.setPosition(advisorLeft,
+		 * advisorBottom);
+		 */
+		CornerAdvisor advisor = new CornerAdvisor("test scren");
+		float padding = 0.01f * DESIGN_HEIGHT;
+		float advisorLeft = DESIGN_WIDTH - advisor.getPrefWidth() - padding;
+		float advisorBottom = DESIGN_HEIGHT - advisor.getPrefHeight() - padding;
+		employeePane.setPosition(DESIGN_WIDTH / 4 + ROSTER_OFFSET,
+				DESIGN_HEIGHT / 2);
+		roster.setPosition(ROSTER_OFFSET, DESIGN_HEIGHT / 2);
+		advisor.setPosition(advisorLeft, advisorBottom);
+		HomeButton home = new HomeButton();
+
+		stage.addActor(home);
+		stage.addActor(advisor);
+		stage.addActor(roster);
+		stage.addActor(employeePane);
+
+	}
+
+	private Actor createEmployeePane()
+	{
+
+		Table employeePane = new Table();
+
+		List<Label> employees = new LinkedList<>();
+
+		Label employee1 = new Label("Employee 1", skin);
+		Label employee2 = new Label("Employee 2", skin);
+		Label employee3 = new Label("Employee 3", skin);
+		Label employee4 = new Label("Employee 4", skin);
+		employees.add(employee1);
+		employees.add(employee2);
+		employees.add(employee3);
+		employees.add(employee4);
+
+		for (Label employee : employees)
+		{
+			employee.setAlignment(Align.center);
+			employeePane.add(employee);
+			employeePane.row();
+		}
+
+		return employeePane;
+	}
+
+	private Actor createEmployeeModel()
+	{
+
+		Table employeeModel = new Table();
+
+		Label employeeName = new Label("Frank Tucker", skin);
+		Label employeePay = new Label("Pay: " + PAY, skin);
+		employeeName.setAlignment(Align.left);
+		employeePay.setAlignment(Align.left);
+		employeeModel.add(employeeName);
+		employeeModel.row();
+		employeeModel.add(employeePay);
+
+		return employeeModel;
+	}
+
+	private Actor createEmploymentTitle(Layout parent, String titleText)
+	{
+		TextButton title = new Button2(titleText, skin, parent);
+		title.align(Align.center);
+		title.setDisabled(true);
+
+		return title;
+	}
+
+	@Override
+	public void dispose()
+	{
+		stage.dispose();
+		this.game = null;
+	}
+
+	@Override
+	public void hide()
+	{
+		Gdx.input.setInputProcessor(null);
+	}
+
+	@Override
+	public void pause()
+	{
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException(
+				"The method is not implemented yet.");
+	}
+
+	@Override
+	public void render(float delta)
+	{
+		stage.act(delta);
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height)
+	{
+		stage.getViewport().update(width, height);
+	}
+
+	@Override
+	public void resume()
+	{
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException(
+				"The method is not implemented yet.");
+	}
+
+	@Override
+	public void show()
+	{
+		Gdx.input.setInputProcessor(stage);
+	}
+
 }
--- core/src/edu/asu/c3simulator/screens/TestScreen.java
+++ core/src/edu/asu/c3simulator/screens/TestScreen.java
@@ -1,4 +1,5 @@
 package edu.asu.c3simulator.screens;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
@@ -12,84 +13,92 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import edu.asu.c3simulator.C3Simulator;
 import edu.asu.c3simulator.widgets.HomeButton;
 import edu.asu.c3simulator.widgets.RandomEvent;
+
 /* 
-* @author Olono, Shantal
-* 
-*/
+ * @author Olono, Shantal
+ * 
+ */
 public class TestScreen implements Screen
-{ 
+{
 	private static final int DESIGN_WIDTH = 1280;
 	private static final int DESIGN_HEIGHT = 720;
 	private static final int DESIGN_SCREEN_CENTER_X = DESIGN_WIDTH / 2;
 	private static final int DESIGN_SCREEN_CENTER_Y = DESIGN_HEIGHT / 2;
-	
+
 	@SuppressWarnings("unused")
 	private Game game;
-	
+
 	private Stage stage;
 	private Skin skin;
-	
 
 	public TestScreen(C3Simulator game)
 	{
 		this.game = game;
-		Viewport stageViewport = new StretchViewport(DESIGN_WIDTH, DESIGN_HEIGHT);
+		Viewport stageViewport = new StretchViewport(DESIGN_WIDTH,
+				DESIGN_HEIGHT);
 		this.stage = new Stage(stageViewport);
 		this.skin = new Skin(Gdx.files.internal("skins/default/uiskin.json"));
 		RandomEvent rand = new RandomEvent();
 		Label employee1 = new Label("test", skin);
 		String newText = "";
-		
-		while(newText.equals("")){
+
+		while (newText.equals(""))
+		{
 			newText = rand.getEvent(10);
 			employee1.setText(newText);
 		}
-		
-		
-		employee1.setPosition(DESIGN_WIDTH/2, DESIGN_HEIGHT/2);
-		  HomeButton home = new HomeButton();
-			
-		  stage.addActor(home);
-		  stage.addActor(employee1);
-		//this.stage.add(rand);
+
+		employee1.setPosition(DESIGN_WIDTH / 2, DESIGN_HEIGHT / 2);
+		HomeButton home = new HomeButton();
+
+		stage.addActor(home);
+		stage.addActor(employee1);
+		// this.stage.add(rand);
 	}
 
 	@Override
-	public void render(float delta) {
+	public void render(float delta)
+	{
 		stage.act(delta);
 		stage.draw();
 	}
 
 	@Override
-	public void resize(int width, int height) {
+	public void resize(int width, int height)
+	{
 		stage.getViewport().update(width, height);
-		
+
 	}
 
 	@Override
-	public void show() {
+	public void show()
+	{
 		Gdx.input.setInputProcessor(stage);
-		
+
 	}
 
 	@Override
-	public void hide() {
+	public void hide()
+	{
 		Gdx.input.setInputProcessor(null);
 	}
 
 	@Override
-	public void pause() {
+	public void pause()
+	{
 		Gdx.input.setInputProcessor(null);
 	}
 
 	@Override
-	public void resume() {
+	public void resume()
+	{
 		Gdx.input.setInputProcessor(stage);
-		
+
 	}
 
 	@Override
-	public void dispose() {
+	public void dispose()
+	{
 		stage.dispose();
 		this.game = null;
 	}
