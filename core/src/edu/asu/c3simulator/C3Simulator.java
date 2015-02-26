7be28db59396aa256fb1f9caec4eb1da1e906e5c
Nick Krogstad <nickkrogstad@gmail.com>
2015-02-25 21:48:08 -0700

    Employment Screen Implementation

--- core/src/edu/asu/c3simulator/C3Simulator.java
+++ core/src/edu/asu/c3simulator/C3Simulator.java
@@ -3,14 +3,14 @@ package edu.asu.c3simulator;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 
-import edu.asu.c3simulator.screens.DifficultySelectionScreen;
+import edu.asu.c3simulator.screens.EmployeeHistoryScreen;
 
 public class C3Simulator extends Game
 {
 	@Override
 	public void create()
 	{
-		Screen firstScreen = new DifficultySelectionScreen(this);
+		Screen firstScreen = new EmployeeHistoryScreen(this);
 		this.setScreen(firstScreen);
 	}
 	
