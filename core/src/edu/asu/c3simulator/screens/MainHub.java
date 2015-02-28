14074f0a7b874b769fc565f39d6e8fdafa0ded33
Justin Reigel <justinreigel89@gmail.com>
2015-02-28 00:25:33 -0700

    Mainhub
    Fixed formatting, properly added front end for playerStatusDisplay.

--- core/src/edu/asu/c3simulator/screens/MainHub.java
+++ core/src/edu/asu/c3simulator/screens/MainHub.java
@@ -23,10 +23,13 @@ import com.badlogic.gdx.scenes.scene2d.utils.Layout;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import edu.asu.c3simulator.simulation.C3Simulation;
 import edu.asu.c3simulator.simulation.Player;
+import edu.asu.c3simulator.util.PlayerImp;
 import edu.asu.c3simulator.widgets.CornerAdvisor;
 import edu.asu.c3simulator.widgets.NavigationPanel;
 import edu.asu.c3simulator.widgets.PlayerStatusDisplay;
+import edu.asu.c3simulator.widgets.PlayerStatusDisplay.Field;
 /**
  * This class displays the main hub, allowing the player to navigate to their Businesses. After: DifficultySelection, HomeButton. 
  * 
@@ -34,36 +37,6 @@ import edu.asu.c3simulator.widgets.PlayerStatusDisplay;
  */
 public class MainHub implements Screen
 {
-	private static class Button2 extends TextButton
-	{
-		Layout parent;
-
-		public Button2(String text, Skin skin, Layout parent)
-		{
-			super(text, skin);
-			this.parent = parent;
-		}
-
-		public float getPrefWidth()
-		{
-			if (parent == null)
-				return super.getPrefWidth();
-			else
-				return parent.getPrefWidth();
-		}
-
-		// @formatter:off
-		/*
-		 * private float getMaxWidth(Table table) { float width = 0.0f;
-		 * 
-		 * for (Cell<?> cell : table.getCells()) { if (cell.getActor()
-		 * instanceof Button2) continue; if (cell.getPrefWidth() > width) width
-		 * = cell.getPrefWidth(); }
-		 * 
-		 * return width; }
-		 */
-		// @formatter:on
-	}
 
 	private static final String ADVISOR_TEXT = "This is a test of TextAreaX. This is intended to cover multiple lines at a width of 200px. This is the second extention";
 	private static final int DESIGN_WIDTH = 1280;
@@ -94,22 +67,24 @@ public class MainHub implements Screen
 
 		choices = new Table();
 		company = Company();
+		
+		AllManagementScreens.initialize(game);
 		//difficultyChoiceEasy = createDifficultyChoiceEasy();
 		//difficultyChoiceHard = createDifficultyChoiceHard();
 		
 		//choices.add(difficultyChoiceHard).top().spaceLeft(75);
 		advisor = new CornerAdvisor(ADVISOR_TEXT, Color.BLACK);
 		navigation = new NavigationPanel(game, skin);
-
+		
 		navigation.addButton("Businesses", game.getScreen() );
-		navigation.addButton("Manage", game.getScreen());
+		navigation.addButton("Manage", AllManagementScreens.RETIRED_PRODUCTS.getInstance());
 		navigation.addButton("Council", game.getScreen());
 		navigation.addSubButton("Businesses", "1.Create New", game.getScreen() );
 		navigation.addSubButton("Businesses", "2.Create New", game.getScreen() );
 		navigation.addSubButton("Businesses", "3.Create New", game.getScreen() );
 		
-		Label playerLabel = new Label( ("Player"), new Label.LabelStyle(font, new Color(Color.DARK_GRAY)) );
-		playerLabel.setPosition(50, 680);
+		Label playerLabel = new Label("Player", skin );
+		playerLabel.setPosition(40, 680);
 		
 		choices.setTransform(true);
 		choices.setPosition(DESIGN_SCREEN_CENTER_X, DESIGN_SCREEN_CENTER_Y);
@@ -119,6 +94,8 @@ public class MainHub implements Screen
 		float advisorBottom = DESIGN_HEIGHT - advisor.getPrefHeight() - padding;
 		advisor.setPosition(advisorLeft, advisorBottom);
 		navigation.setPosition(navigation.getWidth()/2 - padding, 500);
+		System.out.println("Create new home screen");
+
 		
 		stage.addActor(playerLabel);
 		stage.addActor(advisor);
@@ -167,81 +144,20 @@ public class MainHub implements Screen
 	}
 	private Actor playerInfo()
 	{
-		Table playerInfo = new Table();
-		List<Label> lines = new LinkedList<>();
+		//TODO: Implement PlayerStatusDisplay Interface
+		
+		C3Simulation sim = new C3Simulation(){
+			public String getSimulationDate() {
+				return("Jan, Year: 1");
+				}
+		};
+		PlayerStatusDisplay playerStatus = new PlayerStatusDisplay(new PlayerImp("Plebian", 8000, 2000, sim));
+		
+
 		// REFACTOR: Load lines from file
 		//Every second label is a description related to the first
-		
-		
-		Label line1 = new Label(String.format("%-20s %s", "Status: ",  "Plebian"), new Label.LabelStyle(font, new Color(Color.BLACK)));
-		Label line2 = new Label(String.format("%-21s %s", "Net Worth: ",  "8000"), new Label.LabelStyle(font, new Color(Color.BLACK)));
-		//TODO: Fix formatting. As the year increases, the label will push the text to the left of the screen and ruin the formatting.
-		Label line3 = new Label(String.format("%-9s %s", "Date: ",  "January, Year 1"),new Label.LabelStyle(font, new Color(Color.BLACK)));
-		lines.add(line1);
-		lines.add(line2);
-		lines.add(line3);
-		//iterate through the labels and place every second label on the right
-		for (int i = 0; i < lines.size(); i++)
-		{
-			lines.get(i).setAlignment(Align.left);
-			playerInfo.add(lines.get(i));
-			playerInfo.row();
-		}
-		playerInfo.setPosition(200, 600);
-		return playerInfo;
-	}
-
-	private Actor createDifficultyChoiceDescriptionEasy()
-	{
-		// REFACTOR: Combine with #createDifficultyChoiceDescriptionHard
-		Table difficultyChoiceDescriptionEasy = new Table();
-
-		List<Label> lines = new LinkedList<>();
-		// REFACTOR: Load lines from file
-		Label line1 = new Label("$8 000 startup", skin);
-		Label line2 = new Label("Tips Display", skin);
-		Label line3 = new Label("Modified Competition", skin);
-		lines.add(line1);
-		lines.add(line2);
-		lines.add(line3);
-		for (Label line : lines)
-		{
-			line.setAlignment(Align.center);
-			difficultyChoiceDescriptionEasy.add(line);
-			difficultyChoiceDescriptionEasy.row();
-		}
-
-		return difficultyChoiceDescriptionEasy;
-	}
-
-	private Actor createDifficultyChoiceDescriptionHard()
-	{
-		Table difficultyChoiceDescriptionHard = new Table();
-
-		List<Label> lines = new LinkedList<>();
-		Label line1 = new Label("$2 000 startup", skin);
-		Label line2 = new Label("Realistic Competition", skin);
-		lines.add(line1);
-		lines.add(line2);
-		
-
-		for (Label line : lines)
-		{
-			line.setAlignment(Align.center);
-			difficultyChoiceDescriptionHard.add(line);
-			difficultyChoiceDescriptionHard.row();
-		}
-
-		return difficultyChoiceDescriptionHard;
-	}
-
-	private Actor createDifficultyTitle(Layout parent, String titleText)
-	{
-		TextButton title = new Button2(titleText, skin, parent);
-		title.align(Align.center);
-		title.setDisabled(true);
-
-		return title;
+		playerStatus.setPosition(200, 600);
+		return playerStatus;
 	}
 
 	@Override
@@ -268,7 +184,7 @@ public class MainHub implements Screen
 	@Override
 	public void render(float delta)
 	{
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		//Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		stage.act(Gdx.graphics.getDeltaTime());
 		stage.draw();
