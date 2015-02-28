133969522a7966a11fe301e7ba510173581a86cb
malyahy2 <malyahy2@asu.edu>
2015-02-27 19:24:58 -0700

    Finished Task Management
    completely finished task management and fixed a couple of bugs that
    would not move all the items and also leave some items still in the
    checked status

--- core/src/edu/asu/c3simulator/screens/TaskManagement.java
+++ core/src/edu/asu/c3simulator/screens/TaskManagement.java
@@ -1,68 +1,109 @@
 package edu.asu.c3simulator.screens;
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Array;
 
-import edu.asu.c3simulator.widgets.CornerAdvisor;
-import edu.asu.c3simulator.widgets.NavigationPanel;
+import edu.asu.c3simulator.util.Task;
 
-public class TaskManagement implements Screen
+public class TaskManagement extends Table
 {
 	private class addTasksButtonListener extends ClickListener
-	{
-		
-		public addTasksButtonListener()
-		{
-			
-		}
-		
+	{	
 		@Override
 		public void clicked(InputEvent event, float x, float y)
 		{
-			//TODO implement adding of tasks
+			Array<Button> checkedActionBasedTasks = actionBasedTasksGroup.getButtons();
+			int numberOfButtons = checkedActionBasedTasks.size;
+			for(int index=1; index<=numberOfButtons; index++)
+			{
+				Button checkedButton = checkedActionBasedTasks.get(numberOfButtons-index);
+				if(checkedButton.isChecked())
+				{
+					chosenTasksList.addActorAt(0, checkedButton);
+					chosenTasksGroup.add(checkedButton);
+					actionBasedTasksList.removeActor(checkedButton);
+					actionBasedTasksGroup.remove(checkedButton);
+				}
+			}
+			
+			Array<Button> checkedNonActionBasedTasks = nonActionBasedTasksGroup.getButtons();
+			numberOfButtons = checkedNonActionBasedTasks.size;
+			for(int index=1; index<=numberOfButtons; index++)
+			{
+				Button checkedButton = checkedNonActionBasedTasks.get(numberOfButtons-index);
+				if(checkedButton.isChecked())
+				{
+					chosenTasksList.addActorAt(0, checkedButton);
+					chosenTasksGroup.add(checkedButton);
+					nonActionBasedTasksList.removeActor(checkedButton);
+					nonActionBasedTasksGroup.remove(checkedButton);
+				}
+			}
+			actionBasedTasksGroup.uncheckAll();
+			nonActionBasedTasksGroup.uncheckAll();
+			chosenTasksGroup.uncheckAll();
 		}
 	}
 	
 	private class removeTasksButtonListener extends ClickListener
 	{
-		
-		public removeTasksButtonListener()
-		{
-			
-		}
-		
 		@Override
 		public void clicked(InputEvent event, float x, float y)
 		{
-			//TODO implement adding of tasks
+			Array<Button> chosenTasks = chosenTasksGroup.getButtons();
+			int numberOfButtons = chosenTasks.size;
+			for(int index=1; index<=numberOfButtons; index++)
+			{
+				Button checkedButton = chosenTasks.get(numberOfButtons-index);
+				CheckBox checkBoxButton = (CheckBox)checkedButton;
+				if(checkedButton.isChecked())
+				{
+					if(displayedTasks.get(checkBoxButton).getType().equalsIgnoreCase("Action Based"))
+					{
+						actionBasedTasksList.addActor(checkedButton);
+						actionBasedTasksGroup.add(checkedButton);
+					}
+					else
+					{
+						nonActionBasedTasksList.addActor(checkedButton);
+						nonActionBasedTasksGroup.add(checkedButton);
+					}
+					chosenTasksList.removeActor(checkedButton);
+					chosenTasksGroup.remove(checkedButton);
+				}
+			}
+			actionBasedTasksGroup.uncheckAll();
+			nonActionBasedTasksGroup.uncheckAll();
+			chosenTasksGroup.uncheckAll();
 		}
 	}
 	
-	private Game game;
-	private Stage stage;
 	private Skin skin;
-	private List<String> actionBasedTasksList, nonActionBasedTasksList;
-	private List<String> chosenTasksList;
+	private VerticalGroup actionBasedTasksList, nonActionBasedTasksList, chosenTasksList;
+	private ButtonGroup actionBasedTasksGroup, nonActionBasedTasksGroup, chosenTasksGroup;
+	private Hashtable<CheckBox, Task> displayedTasks;
 	
-	public TaskManagement(Game game)
+	public TaskManagement(Stage stage, Skin skin)
 	{
-		this.game = game;
-		this.stage = new Stage();
-		this.skin = new Skin(Gdx.files.internal("skins/default/uiskin.json"));
+		this.skin = skin;
 		
-		Table mainTable = new Table();
-		mainTable.setSize(0.70f*stage.getWidth(), 0.70f*stage.getHeight());
+		displayedTasks =  new Hashtable<CheckBox, Task>();
+		
+		this.setSize(0.70f*stage.getWidth(), 0.70f*stage.getHeight());
 		
 		Table leftSection = new Table();
 		Table rightSection = new Table();
@@ -78,10 +119,14 @@ public class TaskManagement implements Screen
 		TextButton addTasksButton = new TextButton("Add Tasks", skin);
 		addTasksButton.addListener(new addTasksButtonListener());
 		
-		//Label actionBasedTasksList = new Label("Action Based", skin);
-		//Label nonActionBasedTasksList = new Label("Non-Action Based", skin);
-		actionBasedTasksList = new List<String>(skin);
-		nonActionBasedTasksList = new List<String>(skin);
+		actionBasedTasksList = new VerticalGroup();
+		nonActionBasedTasksList = new VerticalGroup();
+		actionBasedTasksGroup = new ButtonGroup();
+		actionBasedTasksGroup.setMaxCheckCount(-1);
+		actionBasedTasksGroup.setMinCheckCount(0);
+		nonActionBasedTasksGroup = new ButtonGroup();
+		nonActionBasedTasksGroup.setMaxCheckCount(-1);
+		nonActionBasedTasksGroup.setMinCheckCount(0);
 		ScrollPane actionBasedTasksScroll = new ScrollPane(actionBasedTasksList, skin);
 		ScrollPane nonActionBasedTasksScroll = new ScrollPane(nonActionBasedTasksList, skin);
 		
@@ -102,120 +147,86 @@ public class TaskManagement implements Screen
 		TextButton tasksListLabel = new TextButton("Chosen Tasks", skin);
 		tasksListLabel.setDisabled(true);
 		tasksListLabel.setColor(0.5f, 0.5f, 0.5f, 1.0f);
-		chosenTasksList = new List<String>(skin);
+		chosenTasksList = new VerticalGroup();
+		chosenTasksGroup = new ButtonGroup();
+		chosenTasksGroup.setMaxCheckCount(-1);
+		chosenTasksGroup.setMinCheckCount(0);
 		ScrollPane chosenTasksScroll = new ScrollPane(chosenTasksList, skin);
 		TextButton removeTasksButton = new TextButton("remove", skin);
-		addTasksButton.addListener(new removeTasksButtonListener());
+		removeTasksButton.addListener(new removeTasksButtonListener());
 		
 		leftSection.add(tasksListLabel).fillX().align(Align.center);
 		leftSection.row();
 		leftSection.add(chosenTasksScroll).expand().fill().center();
 		leftSection.row();
-		leftSection.add(removeTasksButton).center();
+		leftSection.add(removeTasksButton).fillX().center();
 
-		mainTable.add(leftSection).expand().fill();
-		mainTable.add(rightSection).expand().fill();
-		mainTable.setPosition(stage.getWidth()/2 - mainTable.getWidth()/2, stage.getHeight()/2 - mainTable.getHeight()/2);
-		
-		CornerAdvisor advisor = new CornerAdvisor("Test");
-		float padding = 0.01f * stage.getHeight();
-		float advisorLeft = stage.getWidth() - advisor.getPrefWidth() - padding;
-		float advisorBottom = stage.getHeight() - advisor.getPrefHeight() - padding;
-		advisor.setPosition(advisorLeft, advisorBottom);
-		
-		//TODO add screens
-		NavigationPanel navigationPanel = new NavigationPanel(game, skin);
-		navigationPanel.addButton("Company", null);
-		navigationPanel.addSubButton("Company", "Business", null);
-		navigationPanel.addSubButton("Company", "Assets", null);
-		navigationPanel.addButton("Tasks", null);
-		navigationPanel.addSubButton("Tasks", "Manage", null);
-		navigationPanel.addSubButton("Tasks", "Completed", null);
-		navigationPanel.showSubButtonsFor("Tasks");
-		
-		navigationPanel.setPosition(0.01f * stage.getWidth(), stage.getHeight() - (0.3f * stage.getHeight()));
-		
-		mainTable.debug();
-		leftSection.debug();
-		rightSection.debug();
-		actionBased.debug();
-		nonActionBased.debug();
+		this.add(leftSection).expand().fill();
+		this.add(rightSection).expand().fill();
+		this.setPosition(stage.getWidth()/2 - this.getWidth()/2, stage.getHeight()/2 - this.getHeight()/2);
 		
 		updateAvailableTasks();
 		updateChosenTasks();
-		
-		stage.addActor(navigationPanel);
-		stage.addActor(advisor);
-		stage.addActor(mainTable);
 	}
 	
 	public void updateAvailableTasks()
 	{
-		String[] actionBasedList = {"task 1", "task 2", "task 3", "task 4", "task 5"};
-		String[] nonActionBasedList = {"task 6", "task 7", "task 8", "task 9", "task 10"};
-		
+		ArrayList<Task> availableTasks = getAvailableTasks();
 		actionBasedTasksList.clear();
 		nonActionBasedTasksList.clear();
 		
-		actionBasedTasksList.setItems(actionBasedList);
-		nonActionBasedTasksList.setItems(nonActionBasedList);
+		for(Task task : availableTasks)
+		{
+			CheckBox newTaskCheckBox = new CheckBox(" "+task.getName(), skin);
+			displayedTasks.put(newTaskCheckBox, task);
+			if(task.getType().equalsIgnoreCase("Action Based"))
+			{
+				actionBasedTasksList.addActor(newTaskCheckBox);
+				actionBasedTasksGroup.add(newTaskCheckBox);
+			}
+			else
+			{
+				nonActionBasedTasksList.addActor(newTaskCheckBox);
+				nonActionBasedTasksGroup.add(newTaskCheckBox);
+			}
+		}
+		
+		actionBasedTasksList.left();
+		nonActionBasedTasksList.left();
+	}
+	
+	private ArrayList<Task> getAvailableTasks()
+	{
+		String[] actionBasedList = {"task 1.1", "task 2.1", "task 3.1", "task 4.1", "task 5.1", "task 6.1", "task 7.1", "task 8.1", "task 9.1", "task 10.1"};
+		String[] nonActionBasedList = {"task 1.2", "task 2.2", "task 3.2", "task 4.2", "task 5.2", "task 6.2", "task 7.2", "task 8.2", "task 9.2", "task 10.2"};
+		
+		ArrayList<Task> availableTasks = new ArrayList<Task>(); 
+		
+		for(String taskName : actionBasedList)
+			availableTasks.add(new Task(taskName, "Action Based"));
+		
+		for(String taskName : nonActionBasedList)
+			availableTasks.add(new Task(taskName, "Non-Action Based"));
+		
+		return availableTasks;
 	}
 	
 	public void updateChosenTasks()
 	{
-		String[] chosenTasks = {"task 11", "task 12", "task 13", "task 14", "task 15", "task 16",
-								"task 21", "task 22", "task 23", "task 24", "task 25", "task 26",
-								"task 31", "task 32", "task 33", "task 34", "task 35", "task 36"};
-		chosenTasksList.clear();
-		chosenTasksList.setItems(chosenTasks);
+		//TODO get current chosen tasks 
 	}
-
-	@Override
-	public void render(float delta)
+	
+	public ArrayList<Task> getChosenTasks()
 	{
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	    stage.act(Gdx.graphics.getDeltaTime());
-	    stage.draw();
-	}
-
-	@Override
-	public void resize(int width, int height)
-	{
-		stage.getViewport().update(width, height);
-	}
-
-	@Override
-	public void show()
-	{
-		Gdx.input.setInputProcessor(stage);
+		ArrayList<Task> chosenTasks = new ArrayList<Task>(); 
 		
-	}
-
-	@Override
-	public void hide()
-	{
-		Gdx.input.setInputProcessor(null);
+		Array<Button> chosenTaskButtons = chosenTasksGroup.getButtons();
+		for(Button checkedButton : chosenTaskButtons)
+		{
+			CheckBox checkBoxButton = (CheckBox)checkedButton;
+			chosenTasks.add(displayedTasks.get(checkBoxButton));
+		}
 		
-	}
-
-	@Override
-	public void pause()
-	{
-		// TODO Auto-generated method stub
-		//throw new UnsupportedOperationException("The method is not implemented yet.");
-	}
-
-	@Override
-	public void resume()
-	{
-		// TODO Auto-generated method stub
-		//throw new UnsupportedOperationException("The method is not implemented yet.");
-	}
-
-	@Override
-	public void dispose()
-	{
-		stage.dispose();
-		this.game = null;
+		return chosenTasks;
 	}
 }
