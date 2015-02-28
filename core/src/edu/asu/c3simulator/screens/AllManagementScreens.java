668936163467225b05ee06afb878112775267ff1
Justin Reigel <justinreigel89@gmail.com>
2015-02-28 00:24:30 -0700

    AllManagementScreens
    Added premarket and retired products screens. Added lines to initialize
    both screens(Include managePanel).

--- core/src/edu/asu/c3simulator/screens/AllManagementScreens.java
+++ core/src/edu/asu/c3simulator/screens/AllManagementScreens.java
@@ -4,49 +4,48 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 
 /**
- * Enumerated Singleton to hold all sub-screens of the Management Screen. Each enumerated
- * value represents one sub-screen.
+ * Enumerated Singleton to hold all sub-screens of the Management Screen. Each
+ * enumerated value represents one sub-screen.
  * 
- * Note: {@link #initialize(Game)} must be called prior to {@link #getInstance()}.
- * {@link #initialize(Game)} only needs to be called once throughout the lifecycle of the
- * program, but additional calls are harmless.
+ * Note: {@link #initialize(Game)} must be called prior to
+ * {@link #getInstance()}. {@link #initialize(Game)} only needs to be called
+ * once throughout the lifecycle of the program, but additional calls are
+ * harmless.
  * 
  * @author Moore, Zachary
- *
+ * 
  */
 public enum AllManagementScreens
 {
-	PRE_MARKET,
-	CURRENT_PRODUCTS,
-	RETIRED_PRODUCTS,
-	CURRENT_PRODUCTS_GROWTH,
-	GROWTH_DEMAND,
-	EMPLOYMENT,
-	MARKETING;
-	
+	PRE_MARKET, CURRENT_PRODUCTS, RETIRED_PRODUCTS, CURRENT_PRODUCTS_GROWTH, GROWTH_DEMAND, EMPLOYMENT, MARKETING;
+
 	/**
-	 * True if {@link #initialize(Game)} has been called. This value must be true before
-	 * calling {@link #getInstance()} or an {@link IllegalStateException} will be thrown
+	 * True if {@link #initialize(Game)} has been called. This value must be
+	 * true before calling {@link #getInstance()} or an
+	 * {@link IllegalStateException} will be thrown
 	 */
 	private static boolean initialized = false;
-	
+
 	private Screen instance;
-	
+
 	public static void initialize(Game game)
 	{
 		if (!initialized)
 		{
 			initialized = true;
-			PRE_MARKET.instance = null;
+			PRE_MARKET.instance = new PreMarketProducts(game);
 			CURRENT_PRODUCTS.instance = null;
-			RETIRED_PRODUCTS.instance = null;
+			RETIRED_PRODUCTS.instance = new RetiredProducts(game);
 			CURRENT_PRODUCTS_GROWTH.instance = null;
 			GROWTH_DEMAND.instance = null;
 			EMPLOYMENT.instance = null;
 			MARKETING.instance = null;
+			((PreMarketProducts) PRE_MARKET.instance).initialize();
+			((RetiredProducts) RETIRED_PRODUCTS.instance).initialize();
+			// TODO: initialize more as they are created
 		}
 	}
-	
+
 	/**
 	 * @return The instance of this screen.
 	 * @throws IllegalStateException
@@ -55,6 +54,7 @@ public enum AllManagementScreens
 	 */
 	public Screen getInstance()
 	{
+		System.out.println("instance obtained");
 		if (!initialized)
 		{
 			throw new IllegalStateException(
