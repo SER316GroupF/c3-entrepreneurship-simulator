b7f48f648e5e02d18c17e8ef6e4eb3997dcff4d8
Justin Reigel <justinreigel89@gmail.com>
2015-03-03 22:52:31 -0700

    Sell company
    Corrected format and added documentation to sell company.

--- core/src/edu/asu/c3simulator/widgets/SellCompany.java
+++ core/src/edu/asu/c3simulator/widgets/SellCompany.java
@@ -1,7 +1,5 @@
 package edu.asu.c3simulator.widgets;
 
-import java.awt.Color;
-
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -11,71 +9,79 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
-import com.badlogic.gdx.scenes.scene2d.ui.Window.WindowStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
-import edu.asu.c3simulator.screens.MainHub;
-
+/**
+ * Creates a button that in turn creates a popup window when clicked. The popup
+ * window requires confirmation via button click before selling the company.
+ * 
+ * @author Justin
+ *
+ */
 public class SellCompany extends Table
 {
-	
+
 	private TextButton button;
 	private TextButtonStyle textButtonStyle;
 	private BitmapFont font;
 	private Window window;
-	
-	
+
 	public SellCompany(final Game game, final Skin skin)
 	{
 		super();
 		final TextButton continueButton = new TextButton("Continue", skin);
-		continueButton.addListener(new ClickListener(){
+		continueButton.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y)
 			{
 				System.out.println("Continue!");
-				//TODO: Delete business
+				// TODO: Delete actual business
 			}
 		});
-		
+
 		final TextButton cancelButton = new TextButton("Cancel", skin);
-		cancelButton.addListener(new ClickListener(){
+		cancelButton.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y)
 			{
 				System.out.println("Cancel.");
-				if(window != null)
+				if (window != null)
 				{
 					window.remove();
 				}
 			}
 		});
-        button = new TextButton("Sell", skin);
-        add(button);
+		button = new TextButton("Sell", skin);
+		add(button);
+		// TODO: change positioning to the appropriate position for its
+		// destination screen
 		this.setPosition(1100, 500);
-		font = new BitmapFont(Gdx.files.internal("fonts/arial32_superSample.fnt"));
-		button.addListener(new ClickListener(){
+		font = new BitmapFont(
+				Gdx.files.internal("fonts/arial32_superSample.fnt"));
+		button.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y)
 			{
 				window = new Window("Warning", skin);
 				window.defaults().spaceBottom(10);
 				window.row().fill().expandX();
-				window.add("This will permanently sell your business\n and you will not be able to "
-						+ "but it back.\n Are you sure you would like to continue? ").row();
+				window.add(
+						"This will permanently sell your business\n and you will not be able to "
+								+ "but it back.\n Are you sure you would like to continue? ")
+						.row();
 				Table t = new Table();
 				t.add(continueButton).height(window.getPadTop());
 				t.add(cancelButton).height(window.getPadTop());
 				window.add(t);
-				//window.setDebug(true);
+				// window.setDebug(true);
 				window.getButtonTable().right().bottom();
 				window.setPosition(200f, 200f);
 				window.setSize(550f, 200f);
-				window.setColor(1f,1f, 1f, 1f);
+				window.setColor(1f, 1f, 1f, 1f);
 				window.setResizable(false);
 				getStage().addActor(window);
 			}
 		});
-		
-}
+
+	}
 }
