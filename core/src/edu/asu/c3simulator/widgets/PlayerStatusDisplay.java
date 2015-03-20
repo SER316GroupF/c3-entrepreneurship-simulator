a9352a00241df714542b2f77ecfe52fa8909516b
Zach Moore <zcmoore@asu.edu>
2015-03-20 14:44:21 -0700

    Integrate PlayerStatusDisplay

---
+++ core/src/edu/asu/c3simulator/widgets/PlayerStatusDisplay.java
@@ -0,0 +1,196 @@
+package edu.asu.c3simulator.widgets;
+
+import java.util.HashMap;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+
+import edu.asu.c3simulator.simulation.C3Simulation;
+import edu.asu.c3simulator.simulation.Player;
+import edu.asu.c3simulator.util.ObservationListener;
+
+public class PlayerStatusDisplay extends Table
+{
+	/**
+	 * All fields that are displayed by this widget.
+	 * 
+	 * @author Moore, Zachary
+	 *
+	 */
+	public static enum Field
+	{
+		STATUS("Status"),
+		NET_WORTH("Net Worth"),
+		CAPITAL("Capital"),
+		DATE("Date");
+		
+		private String displayName;
+		
+		private Field(String displayName)
+		{
+			this.displayName = displayName;
+		}
+		
+		public String displayName()
+		{
+			return displayName;
+		}
+	}
+	
+	/**
+	 * Updates this display when the state of the player is changed.
+	 * 
+	 * @author Moore, Zachary
+	 *
+	 */
+	private class PlayerListener implements ObservationListener<Player>
+	{
+		@Override
+		public void stateChanged(Player newState)
+		{
+			setFields(newState);
+		}
+	}
+	
+	private HashMap<Field, String> fieldValues;
+	private boolean updatesEnabled;
+	
+	public PlayerStatusDisplay(Player player)
+	{
+		super();
+		this.fieldValues = new HashMap<>();
+		this.setFields(player);
+		// debug();
+		enableUpdates();
+		Tables.allignColumns(this, 0, Align.left);
+		// TODO: abstract, use percentages; make less hacky
+		float verticalPadding = 3.5f;
+		Tables.padColumn(this, 0, new Padding(0, 50, verticalPadding, verticalPadding));
+		Tables.padColumn(this, 1, new Padding(0, 0, verticalPadding, verticalPadding));
+		
+		player.registerObservationListener(new PlayerListener());
+	}
+	
+	/**
+	 * Given a field, insert 2 text fields into this display to show the name of the field
+	 * followed by the value of the field, as given by {@link #fieldValues}
+	 * 
+	 * @param field
+	 *            Data to insert
+	 */
+	private void insertValuePair(Field field)
+	{
+		String labelText = field.displayName();
+		String valueText = fieldValues.get(field);
+		
+		if (valueText == null)
+			valueText = "";
+		
+		SimpleTextField label = new SimpleTextField(labelText);
+		SimpleTextField valueLabel = new SimpleTextField(valueText);
+		
+		add(label);
+		add(valueLabel);
+	}
+	
+	/**
+	 * Calls {@link #insertValuePair(Field)} followed by {@link #row()}
+	 * 
+	 * @param field
+	 *            Data to insert
+	 */
+	private void insertValuePairRow(Field field)
+	{
+		insertValuePair(field);
+		row();
+	}
+	
+	/**
+	 * Ensures the display shows the same data as found in {@link #fieldValues}
+	 */
+	private void updateTableValues()
+	{
+		if (updatesEnabled)
+		{
+			this.clearChildren();
+			
+			insertValuePairRow(Field.STATUS);
+			insertValuePairRow(Field.NET_WORTH);
+			insertValuePairRow(Field.CAPITAL);
+			insertValuePair(Field.DATE);
+		}
+	}
+	
+	public void setField(Field field, double value)
+	{
+		String valueString = Double.toString(value);
+		setField(field, valueString);
+	}
+	
+	public void setField(Field field, float value)
+	{
+		String valueString = Float.toString(value);
+		setField(field, valueString);
+	}
+	
+	public void setField(Field field, int value)
+	{
+		String valueString = Integer.toString(value);
+		setField(field, valueString);
+	}
+	
+	public void setField(Field field, long value)
+	{
+		String valueString = Long.toString(value);
+		setField(field, valueString);
+	}
+	
+	public void setField(Field field, String value)
+	{
+		if (!value.equals(fieldValues.get(field)))
+		{
+			fieldValues.put(field, value);
+			updateTableValues();
+		}
+	}
+	
+	/**
+	 * Updates the data in {@link #fieldValues} to match the values in player, then
+	 * updates the display after all data updates have been made.
+	 * 
+	 * @param player
+	 *            Data to conform to.
+	 */
+	public void setFields(Player player)
+	{
+		disableUpdates();
+		{ // START non-updating block
+			setField(Field.STATUS, player.getStatus().asString());
+			setField(Field.CAPITAL, "$" + player.getCapital());
+			setField(Field.NET_WORTH, "$" + player.getNetWorth());
+			
+			C3Simulation simulation = player.getSimulation();
+			String simulationDate = simulation.getSimulationDateString();
+			setField(Field.DATE, simulationDate);
+		} // END non-updating block
+		enableUpdates();
+	}
+	
+	/**
+	 * Allow the display to update based on the data in {@link #fieldValues}
+	 */
+	private void enableUpdates()
+	{
+		this.updatesEnabled = true;
+		updateTableValues();
+	}
+	
+	/**
+	 * Disable updating of the display. This will cause {@link #updateTableValues()} to do
+	 * nothing, until updates are enabled again.
+	 */
+	private void disableUpdates()
+	{
+		this.updatesEnabled = false;
+	}
+}
