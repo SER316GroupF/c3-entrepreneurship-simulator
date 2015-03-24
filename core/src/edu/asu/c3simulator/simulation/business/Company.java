06eef333dfe7ec3517b16fa49eb1f63487bcffcd
Zach Moore <zcmoore@asu.edu>
2015-03-24 15:58:59 -0700

    Documentation

--- core/src/edu/asu/c3simulator/simulation/business/Company.java
+++ core/src/edu/asu/c3simulator/simulation/business/Company.java
@@ -4,10 +4,30 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Data object to store data related to, and simulate conduct by a Company. In this model,
+ * a company is made up of its employees, assets (including capital, credit, inventory,
+ * and other non-liquid assets), {@link ConsumerConnection}s, products, and activity (e.g.
+ * marketing campaigns, purchase orders, etc).
+ * <p>
+ * NOTE: {@link Company} assumes that instances of {@link Product} are unique, and specify
+ * a type of product rather than a single manufactured unit.
+ * 
+ * @author Moore, Zachary
+ *
+ */
 public class Company
 {
+	private String name;
 	private List<Employee> employees;
+	
+	/**
+	 * Products currently manufactured and sold by this company, paired with the
+	 * production information about them. If a product is sold but not manufactured, the
+	 * associated value will be null.
+	 */
 	private HashMap<Product, ProductionInformation> productLine;
+	
 	private HashMap<Product, Integer> inventory;
 	private HashMap<Product, Integer> salesInCurrentMonth;
 	private HashMap<Product, Float> retailPrice;
@@ -18,6 +38,23 @@ public class Company
 	private HashMap<Product, Integer> publicOpinionOfProducts;
 	private int publicOpinionOfCompany;
 	
+	/**
+	 * Simulates the sale of a product, by calculating profits and adding it to the
+	 * company's current capital.
+	 * <p>
+	 * This method is intended to be called by Simulation and its agents, as part of a
+	 * market/economy simulation.
+	 * 
+	 * @param product
+	 *            The product to be bought/sold
+	 * @param quantity
+	 *            The quantity to buy/sell
+	 * @return True if the sale was successful, false if the sale was unable to be
+	 *         processed. Note, a false return is indicitive of the quantity being too
+	 *         high for this company to support a sale. For instance, the quantity may
+	 *         cause the company to exceed its monthly sales potential, as specified by
+	 *         {@link #getMonthlySalePotential(Product)}
+	 */
 	public boolean registerSale(Product product, int quantity)
 	{
 		int salesInCurrentMonth = getThisMonthsSalesOf(product) + quantity;
@@ -35,6 +72,14 @@ public class Company
 		}
 	}
 	
+	/**
+	 * If {@link #salesInCurrentMonth} does not contain the specified product, the product
+	 * will be added to the map and initialized with a value of 0, and that value will be
+	 * returned.
+	 * <p>
+	 * Otherwise, the corresponding value in {@link #salesInCurrentMonth} will be
+	 * returned.
+	 */
 	private int getThisMonthsSalesOf(Product product)
 	{
 		if (salesInCurrentMonth.containsKey(product))
@@ -48,6 +93,15 @@ public class Company
 		}
 	}
 	
+	/**
+	 * Equivalent to {@link #retailPrice}, but handles the case in which
+	 * {@link #retailPrice} is not registered for the given product.
+	 * <p>
+	 * As of version 0.1, all products must register a retail price, so an
+	 * {@link IllegalStateException} is thrown if {@link #retailPrice} does not contain
+	 * the given product. However, this functionality may be changed in future revisions
+	 * and is subject to change.
+	 */
 	private float getRetailPriceOf(Product product)
 	{
 		if (retailPrice.containsKey(product))
@@ -119,6 +173,13 @@ public class Company
 		}
 	}
 	
+	/**
+	 * If {@link #inventory} does not contain the specified product, the product will be
+	 * added to the map and initialized with a value of 0, and that value will be
+	 * returned.
+	 * <p>
+	 * Otherwise, the corresponding value in {@link #inventory} will be returned.
+	 */
 	private int getInventoryOf(Product product)
 	{
 		if (inventory.containsKey(product))
@@ -132,6 +193,14 @@ public class Company
 		}
 	}
 	
+	/**
+	 * If {@link #publicOpinionOfProducts} does not contain the specified product, the
+	 * product will be added to the map and initialized with a value equal to
+	 * {@link #publicOpinionOfCompany}, and that value will be returned.
+	 * <p>
+	 * Otherwise, the corresponding value in {@link #publicOpinionOfProducts} will be
+	 * returned.
+	 */
 	private int getPublicOpinionOf(Product product)
 	{
 		if (publicOpinionOfProducts.containsKey(product))
@@ -145,6 +214,9 @@ public class Company
 		}
 	}
 	
+	/**
+	 * @return The total compensation of all employees in {@link #employees}
+	 */
 	private float getMonthlyEmployeeExpense()
 	{
 		float expense = 0;
@@ -157,6 +229,9 @@ public class Company
 		return expense;
 	}
 	
+	/**
+	 * @return The total of all marketing expenses specified in {@link #activeMarketing}
+	 */
 	private float getMonthlyMarketingExpense()
 	{
 		float total = 0;
@@ -169,6 +244,23 @@ public class Company
 		return total;
 	}
 	
+	/**
+	 * Calculates the maximum number of sales a company can process of the given product
+	 * at the current point in time. This differs from
+	 * {@link #getMonthlySalePotential(Product)} in that this method returns how many
+	 * units are ready to be sold, at the current point in time, whereas
+	 * {@link #getSaleCapacity(Product)} returns how many units can be handled by this
+	 * company's {@link ConsumerConnection}s.
+	 * <p>
+	 * Generally, a sale cannot be made unless it is below the remainingSalePotential, AND
+	 * less than or equal to the saleCapacity of that product.
+	 * 
+	 * @see #getMonthlySalePotential(Product)
+	 * @param product
+	 *            Product for which the sale capacity will be calculated
+	 * @return The number of units of the specified product that are ready to be sold at
+	 *         the current point in time
+	 */
 	public int getSaleCapacity(Product product)
 	{
 		if (inventory.containsKey(product))
@@ -181,6 +273,22 @@ public class Company
 		}
 	}
 	
+	/**
+	 * Calculates the maximum number of sales a company can process of the given product
+	 * in a single month. This differs from {@link #getSaleCapacity(Product)} in that this
+	 * method returns how many units can be handled by this company's
+	 * {@link ConsumerConnection}s, whereas {@link #getSaleCapacity(Product)} returns how
+	 * many units are ready to be sold, at the current point in time.
+	 * <p>
+	 * Generally, a sale cannot be made unless it is below the remainingSalePotential, AND
+	 * less than or equal to the saleCapacity of that product.
+	 * 
+	 * @see #getSaleCapacity(Product)
+	 * @param product
+	 *            Product for which the sale potential will be calculated
+	 * @return The maximum number of units of the specified product that can be sold by
+	 *         this company in a single month
+	 */
 	public int getMonthlySalePotential(Product product)
 	{
 		int potential = 0;
@@ -193,6 +301,12 @@ public class Company
 		return potential;
 	}
 	
+	/**
+	 * @param product
+	 *            Product for which the remaining sale potential will be calculated
+	 * @return How many more units of the specified product can be sold by this company in
+	 *         the current month
+	 */
 	public int getRemainingSalePotential(Product product)
 	{
 		int potential = getMonthlySalePotential(product);
@@ -200,6 +314,61 @@ public class Company
 				.get(product) : 0;
 		
 		return potential - used;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public List<Employee> getEmployees()
+	{
+		return employees;
+	}
+	
+	public HashMap<Product, ProductionInformation> getProductLine()
+	{
+		return productLine;
+	}
+	
+	public HashMap<Product, Integer> getInventory()
+	{
+		return inventory;
+	}
+	
+	public HashMap<Product, Integer> getSalesInCurrentMonth()
+	{
+		return salesInCurrentMonth;
+	}
+	
+	public HashMap<Product, Float> getRetailPrice()
+	{
+		return retailPrice;
+	}
+	
+	public Credit getCredit()
+	{
+		return credit;
+	}
+	
+	public List<MarketingCampaign> getActiveMarketing()
+	{
+		return activeMarketing;
+	}
+	
+	public List<ConsumerConnection> getSalesOutlets()
+	{
+		return salesOutlets;
+	}
+	
+	public HashMap<Product, Integer> getPublicOpinionOfProducts()
+	{
+		return publicOpinionOfProducts;
+	}
+	
+	public int getPublicOpinionOfCompany()
+	{
+		return publicOpinionOfCompany;
 	}
 	
 }
