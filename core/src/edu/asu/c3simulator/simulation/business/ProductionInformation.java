82b20d39c0203a81d76803413d3c908aa5a3ec12
Zach Moore <zcmoore@asu.edu>
2015-03-25 08:50:30 -0700

    Added Constructor

--- core/src/edu/asu/c3simulator/simulation/business/ProductionInformation.java
+++ core/src/edu/asu/c3simulator/simulation/business/ProductionInformation.java
@@ -17,6 +17,41 @@ public class ProductionInformation
 	/** Amount spent on materials per unit produced */
 	private float materialBudgetPerUnit;
 	
+	/**
+	 * @param target
+	 *            Product for which this information applies. Must be non-null
+	 * @param monthlyProduction
+	 *            Number of units of {@link #target} to be produced by this company each
+	 *            month. A negative monthlyProduction indicates that the company is
+	 *            destroying units in their inventory.
+	 * @param materialBudgetPerUnit
+	 *            Amount spent on materials per unit produced. Must be >= 0
+	 */
+	public ProductionInformation(Product target, int monthlyProduction,
+			float materialBudgetPerUnit)
+	{
+		super();
+		
+		validateParameters(target, monthlyProduction, materialBudgetPerUnit);
+		
+		this.target = target;
+		this.monthlyProduction = monthlyProduction;
+		this.materialBudgetPerUnit = materialBudgetPerUnit;
+	}
+	
+	private void validateParameters(Product target, int monthlyProduction,
+			float materialBudgetPerUnit)
+	{
+		if (target == null)
+		{
+			throw new IllegalArgumentException("Product must be non-null");
+		}
+		else if (materialBudgetPerUnit < 0)
+		{
+			throw new IllegalArgumentException("Budget cannot be negative");
+		}
+	}
+	
 	public int getMonthlyProduction()
 	{
 		return monthlyProduction;
