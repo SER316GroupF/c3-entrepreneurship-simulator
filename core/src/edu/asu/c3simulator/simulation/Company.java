f5653003c83b718e60cf24e4a752930bdde38b57
Zach Moore <zcmoore@asu.edu>
2015-03-26 15:58:41 -0700

    Documentation Formatting

--- core/src/edu/asu/c3simulator/simulation/Company.java
+++ core/src/edu/asu/c3simulator/simulation/Company.java
@@ -3,9 +3,10 @@ package edu.asu.c3simulator.simulation;
 import java.util.List;
 
 /**
- * @author nickkrogstad
  * Data representation of companies, which hold employees, have capital, 
  * and affect the global market.
+ * 
+ * @author nickkrogstad
  */
 public interface Company
 {
