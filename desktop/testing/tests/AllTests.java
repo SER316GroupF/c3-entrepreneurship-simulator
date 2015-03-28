bd70bf744785b170a66d85453e0b87ea76c3c947
Zach Moore <zcmoore@asu.edu>
2015-03-28 11:26:28 -0700

    Append Tests to Suite

--- desktop/testing/tests/AllTests.java
+++ desktop/testing/tests/AllTests.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestColorDecider.class, TestHistogram.class, TestPieChart.class })
+@SuiteClasses({ TestColorDecider.class, TestHistogram.class, TestPieChart.class,
+		TestCredit.class, TestCompany.class })
 public class AllTests
 {
 	
