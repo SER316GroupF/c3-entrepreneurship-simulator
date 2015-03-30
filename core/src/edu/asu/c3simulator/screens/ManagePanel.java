66ee77390c558d8c93f981b7a7d6d3b504be0478
Justin Reigel <justinreigel89@gmail.com>
2015-03-30 10:54:48 -0700

    ManagePanel
    Updated ManagePanel for aesthetic purposes. Previous managePanel would
    slightly jump around the screen.

--- core/src/edu/asu/c3simulator/screens/ManagePanel.java
+++ core/src/edu/asu/c3simulator/screens/ManagePanel.java
@@ -5,6 +5,7 @@ package edu.asu.c3simulator.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
@@ -16,7 +17,7 @@ import edu.asu.c3simulator.widgets.NavigationPanel;
  * @author Reigel, Justin
  * 
  */
-public class ManagePanel extends Table
+public class ManagePanel
 {
 	private static final int DESIGN_WIDTH = 1280;
 	private static final int DESIGN_HEIGHT = 720;
@@ -24,15 +25,15 @@ public class ManagePanel extends Table
 	private static NavigationPanel navigation;
 	private Skin skin;
 
-	public ManagePanel(Game game)
+	public ManagePanel(Game game, Stage stage)
 	{
 		skin = new Skin(Gdx.files.internal("skins/default/uiskin.json"));
 		navigation = new NavigationPanel(game, skin);
 		// Main buttons for manage panel
 		navigation.addButton("Products", null);
 		navigation.addButton("Growth", null);
-		navigation.addButton("Employment", null);
-		navigation.addButton("Marketing", null);
+		navigation.addButton("Employment", AllManagementScreens.EMPLOYMENT.getInstance());
+		navigation.addButton("Marketing", AllManagementScreens.MARKETING.getInstance());
 		// Products Sub Buttons
 		navigation.addSubButton("Products", "Pre-Market",
 				AllManagementScreens.PRE_MARKET.getInstance());
@@ -43,10 +44,13 @@ public class ManagePanel extends Table
 		navigation.addSubButton("Products", "Product History",
 				AllManagementScreens.PRODUCT_HISTORY.getInstance());
 		// Growth Sub Buttons
-		navigation.addSubButton("Growth", "Current Products Growth", null);
+		navigation.addSubButton("Growth", "Supply", null);
 		navigation.addSubButton("Growth", "Demand", null);
+		
+		navigation.setPosition(0.01f * stage.getWidth(), stage.getHeight() * .7f);
 
-		this.add(navigation);
-		this.setPosition(navigation.getWidth() / 2 + 70, 450);
+		stage.addActor(navigation);
+		//TODO: Fix magic numbers
+		
 	}
 }
