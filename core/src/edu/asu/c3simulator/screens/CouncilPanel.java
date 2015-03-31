160499b46741b2b4bae07ba0159fad8ce159abaf
Justin Reigel <justinreigel89@gmail.com>
2015-03-30 17:38:20 -0700

    Council Panel
    Formatted and added documentation.

--- core/src/edu/asu/c3simulator/screens/CouncilPanel.java
+++ core/src/edu/asu/c3simulator/screens/CouncilPanel.java
@@ -7,6 +7,12 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 import edu.asu.c3simulator.widgets.NavigationPanel;
 
+/**
+ * Navigation Panel for council screens
+ * 
+ * @author Alyahya, Mohammed
+ * 
+ */
 public class CouncilPanel
 {
 	private static final int DESIGN_WIDTH = 1280;
@@ -18,20 +24,23 @@ public class CouncilPanel
 	public CouncilPanel(Game game, Stage stage)
 	{
 		skin = new Skin(Gdx.files.internal("skins/default/uiskin.json"));
-		
+
 		NavigationPanel navigationPanel = new NavigationPanel(game, skin);
 		navigationPanel.addButton("Company", null);
-		navigationPanel.addSubButton("Company", "Business", AllManagementScreens.COMPANY_PANEL.getInstance());
+		navigationPanel.addSubButton("Company", "Business",
+				AllManagementScreens.COMPANY_PANEL.getInstance());
 		navigationPanel.addSubButton("Company", "Assets", null);
 		navigationPanel.addButton("Tasks", null);
-		navigationPanel.addSubButton("Tasks", "Manage", AllManagementScreens.TASK_MANAGEMENT.getInstance());
-		navigationPanel.addSubButton("Tasks", "Completed", AllManagementScreens.COMPLETED_TASKS.getInstance());
+		navigationPanel.addSubButton("Tasks", "Manage",
+				AllManagementScreens.TASK_MANAGEMENT.getInstance());
+		navigationPanel.addSubButton("Tasks", "Completed",
+				AllManagementScreens.COMPLETED_TASKS.getInstance());
 		navigationPanel.showSubButtonsFor("Tasks");
-		
+
 		navigationPanel.setPosition(0.01f * stage.getWidth(), stage.getHeight()
 				- (0.3f * stage.getHeight()));
-		
+
 		stage.addActor(navigationPanel);
-		
+
 	}
 }
